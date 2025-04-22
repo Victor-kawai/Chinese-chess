87e32da6d5a44b4eeab033799baf7d7136b41511
Victor-kawai <1900017878@pku.edu.cn>
2025-04-22 16:36:23 +0800

    git try

---
+++ chinese_chess/test.py
@@ -0,0 +1,10 @@
+'''
+Author: Victor-kawai 1900017878@pku.edu.cn
+Date: 2025-04-22 16:18:03
+LastEditors: Victor-kawai 1900017878@pku.edu.cn
+LastEditTime: 2025-04-22 16:22:56
+FilePath: \Chinese-chess\chinese_chess\test.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import pandas as pd
+print("hello chess!")
