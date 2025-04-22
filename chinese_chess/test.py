17090f9f578f139bee55a24dbfd1ba8af1613232
Victor-kawai <1900017878@pku.edu.cn>
2025-04-22 16:38:52 +0800

    debug

--- chinese_chess/test.py
+++ chinese_chess/test.py
@@ -2,9 +2,37 @@
 Author: Victor-kawai 1900017878@pku.edu.cn
 Date: 2025-04-22 16:18:03
 LastEditors: Victor-kawai 1900017878@pku.edu.cn
-LastEditTime: 2025-04-22 16:22:56
+LastEditTime: 2025-04-22 16:38:26
 FilePath: \Chinese-chess\chinese_chess\test.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 import pandas as pd
-print("hello chess!")
+
+# 车
+class Rook:
+    pass
+# 马
+class Horse:
+    pass
+# 象
+class Elephant:
+    pass
+# 士
+class Guard:
+    pass
+# 将
+class General:
+    pass
+# 兵
+class Soldier:
+    pass
+# 炮
+class Cannon:
+    pass
+
+if __name__ == "__main__":
+    rows = 10
+    columns = 9
+    # 棋盘
+
+    # 棋子
